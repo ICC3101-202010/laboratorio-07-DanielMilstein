3689f28574af2f3a39c85718e318618175cea745
DanielMilstein <dnmilstein@miuandes.cl>
2020-05-19 21:43:31 -0400

    Probar commit remoto.

---
+++ Lab7/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace Lab7
+{
+    partial class Calculadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AC = new System.Windows.Forms.Button();
+            this.del = new System.Windows.Forms.Button();
+            this.Div = new System.Windows.Forms.Button();
+            this.mult = new System.Windows.Forms.Button();
+            this.sum = new System.Windows.Forms.Button();
+            this.subs = new System.Windows.Forms.Button();
+            this.ans = new System.Windows.Forms.Button();
+            this.equal = new System.Windows.Forms.Button();
+            this.n6 = new System.Windows.Forms.Button();
+            this.n5 = new System.Windows.Forms.Button();
+            this.n3 = new System.Windows.Forms.Button();
+            this.n2 = new System.Windows.Forms.Button();
+            this.n4 = new System.Windows.Forms.Button();
+            this.comma = new System.Windows.Forms.Button();
+            this.n1 = new System.Windows.Forms.Button();
+            this.n9 = new System.Windows.Forms.Button();
+            this.n0 = new System.Windows.Forms.Button();
+            this.n8 = new System.Windows.Forms.Button();
+            this.n7 = new System.Windows.Forms.Button();
+            this.display = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AC
+            // 
+            this.AC.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AC.Location = new System.Drawing.Point(452, 246);
+            this.AC.Name = "AC";
+            this.AC.Size = new System.Drawing.Size(76, 70);
+            this.AC.TabIndex = 0;
+            this.AC.Text = "AC";
+            this.AC.UseVisualStyleBackColor = true;
+            // 
+            // del
+            // 
+            this.del.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.del.Location = new System.Drawing.Point(534, 248);
+            this.del.Name = "del";
+            this.del.Size = new System.Drawing.Size(76, 70);
+            this.del.TabIndex = 1;
+            this.del.Text = "DEL";
+            this.del.UseVisualStyleBackColor = true;
+            // 
+            // Div
+            // 
+            this.Div.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Div.Location = new System.Drawing.Point(452, 322);
+            this.Div.Name = "Div";
+            this.Div.Size = new System.Drawing.Size(76, 70);
+            this.Div.TabIndex = 2;
+            this.Div.Text = "/";
+            this.Div.UseVisualStyleBackColor = true;
+            // 
+            // mult
+            // 
+            this.mult.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mult.Location = new System.Drawing.Point(534, 324);
+            this.mult.Name = "mult";
+            this.mult.Size = new System.Drawing.Size(76, 70);
+            this.mult.TabIndex = 3;
+            this.mult.Text = "*";
+            this.mult.UseVisualStyleBackColor = true;
+            // 
+            // sum
+            // 
+            this.sum.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sum.Location = new System.Drawing.Point(452, 398);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(76, 70);
+            this.sum.TabIndex = 4;
+            this.sum.Text = "+";
+            this.sum.UseVisualStyleBackColor = true;
+            // 
+            // subs
+            // 
+            this.subs.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subs.Location = new System.Drawing.Point(534, 400);
+            this.subs.Name = "subs";
+            this.subs.Size = new System.Drawing.Size(76, 70);
+            this.subs.TabIndex = 5;
+            this.subs.Text = "-";
+            this.subs.UseVisualStyleBackColor = true;
+            // 
+            // ans
+            // 
+            this.ans.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ans.Location = new System.Drawing.Point(452, 474);
+            this.ans.Name = "ans";
+            this.ans.Size = new System.Drawing.Size(76, 70);
+            this.ans.TabIndex = 6;
+            this.ans.Text = "ANS";
+            this.ans.UseVisualStyleBackColor = true;
+            // 
+            // equal
+            // 
+            this.equal.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.equal.Location = new System.Drawing.Point(534, 476);
+            this.equal.Name = "equal";
+            this.equal.Size = new System.Drawing.Size(76, 70);
+            this.equal.TabIndex = 7;
+            this.equal.Text = "=";
+            this.equal.UseVisualStyleBackColor = true;
+            // 
+            // n6
+            // 
+            this.n6.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n6.Location = new System.Drawing.Point(346, 322);
+            this.n6.Name = "n6";
+            this.n6.Size = new System.Drawing.Size(76, 70);
+            this.n6.TabIndex = 11;
+            this.n6.Text = "6";
+            this.n6.UseVisualStyleBackColor = true;
+            // 
+            // n5
+            // 
+            this.n5.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n5.Location = new System.Drawing.Point(246, 322);
+            this.n5.Name = "n5";
+            this.n5.Size = new System.Drawing.Size(76, 70);
+            this.n5.TabIndex = 10;
+            this.n5.Text = "5";
+            this.n5.UseVisualStyleBackColor = true;
+            // 
+            // n3
+            // 
+            this.n3.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n3.Location = new System.Drawing.Point(346, 398);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(76, 70);
+            this.n3.TabIndex = 9;
+            this.n3.Text = "3";
+            this.n3.UseVisualStyleBackColor = true;
+            // 
+            // n2
+            // 
+            this.n2.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n2.Location = new System.Drawing.Point(248, 398);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(76, 70);
+            this.n2.TabIndex = 8;
+            this.n2.Text = "2";
+            this.n2.UseVisualStyleBackColor = true;
+            // 
+            // n4
+            // 
+            this.n4.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n4.Location = new System.Drawing.Point(148, 322);
+            this.n4.Name = "n4";
+            this.n4.Size = new System.Drawing.Size(76, 70);
+            this.n4.TabIndex = 15;
+            this.n4.Text = "4";
+            this.n4.UseVisualStyleBackColor = true;
+            // 
+            // comma
+            // 
+            this.comma.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comma.Location = new System.Drawing.Point(346, 474);
+            this.comma.Name = "comma";
+            this.comma.Size = new System.Drawing.Size(76, 70);
+            this.comma.TabIndex = 14;
+            this.comma.Text = ",";
+            this.comma.UseVisualStyleBackColor = true;
+            // 
+            // n1
+            // 
+            this.n1.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n1.Location = new System.Drawing.Point(148, 398);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(76, 70);
+            this.n1.TabIndex = 13;
+            this.n1.Text = "1";
+            this.n1.UseVisualStyleBackColor = true;
+            // 
+            // n9
+            // 
+            this.n9.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n9.Location = new System.Drawing.Point(346, 246);
+            this.n9.Name = "n9";
+            this.n9.Size = new System.Drawing.Size(76, 70);
+            this.n9.TabIndex = 12;
+            this.n9.Text = "9";
+            this.n9.UseVisualStyleBackColor = true;
+            // 
+            // n0
+            // 
+            this.n0.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n0.Location = new System.Drawing.Point(248, 474);
+            this.n0.Name = "n0";
+            this.n0.Size = new System.Drawing.Size(76, 70);
+            this.n0.TabIndex = 19;
+            this.n0.Text = "0";
+            this.n0.UseVisualStyleBackColor = true;
+            // 
+            // n8
+            // 
+            this.n8.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n8.Location = new System.Drawing.Point(248, 246);
+            this.n8.Name = "n8";
+            this.n8.Size = new System.Drawing.Size(76, 70);
+            this.n8.TabIndex = 17;
+            this.n8.Text = "8";
+            this.n8.UseVisualStyleBackColor = true;
+            // 
+            // n7
+            // 
+            this.n7.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n7.Location = new System.Drawing.Point(148, 246);
+            this.n7.Name = "n7";
+            this.n7.Size = new System.Drawing.Size(76, 70);
+            this.n7.TabIndex = 16;
+            this.n7.Text = "7";
+            this.n7.UseVisualStyleBackColor = true;
+            // 
+            // display
+            // 
+            this.display.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.display.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.display.Location = new System.Drawing.Point(144, 120);
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(466, 108);
+            this.display.TabIndex = 20;
+            // 
+            // Calculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(798, 870);
+            this.Controls.Add(this.display);
+            this.Controls.Add(this.n0);
+            this.Controls.Add(this.n8);
+            this.Controls.Add(this.n7);
+            this.Controls.Add(this.n4);
+            this.Controls.Add(this.comma);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.n9);
+            this.Controls.Add(this.n6);
+            this.Controls.Add(this.n5);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.equal);
+            this.Controls.Add(this.ans);
+            this.Controls.Add(this.subs);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.mult);
+            this.Controls.Add(this.Div);
+            this.Controls.Add(this.del);
+            this.Controls.Add(this.AC);
+            this.Name = "Calculadora";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AC;
+        private System.Windows.Forms.Button del;
+        private System.Windows.Forms.Button Div;
+        private System.Windows.Forms.Button mult;
+        private System.Windows.Forms.Button sum;
+        private System.Windows.Forms.Button subs;
+        private System.Windows.Forms.Button ans;
+        private System.Windows.Forms.Button equal;
+        private System.Windows.Forms.Button n6;
+        private System.Windows.Forms.Button n5;
+        private System.Windows.Forms.Button n3;
+        private System.Windows.Forms.Button n2;
+        private System.Windows.Forms.Button n4;
+        private System.Windows.Forms.Button comma;
+        private System.Windows.Forms.Button n1;
+        private System.Windows.Forms.Button n9;
+        private System.Windows.Forms.Button n0;
+        private System.Windows.Forms.Button n8;
+        private System.Windows.Forms.Button n7;
+        private System.Windows.Forms.Label display;
+    }
+}
+
